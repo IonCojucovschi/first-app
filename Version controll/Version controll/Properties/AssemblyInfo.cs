98ee7fb13bf16a501b01976a985a487cad6ec879
IonCojucovschi <ion.cojucovschi@outlook.com>
2017-06-28 08:23:10 -0700

    nr1

--- Version controll/Version controll/Properties/AssemblyInfo.cs
+++ Version controll/Version controll/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApp1")]
+[assembly: AssemblyTitle("Version controll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp1")]
+[assembly: AssemblyProduct("Version controll")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("01c40ce7-b2e3-4676-8380-02154b88742c")]
+[assembly: Guid("a4f87d83-1c52-4954-af80-15eb6a17b88f")]
 
 // Version information for an assembly consists of the following four values:
 //
